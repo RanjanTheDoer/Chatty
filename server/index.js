b2172803b5a13096e584cab044d8d9e17f8286b6
Priyanshu <myselfpriyanshu1@gmail.com>
2023-12-26 23:38:13 +0530

    commiting

--- server/index.js
+++ server/index.js
@@ -8,11 +8,12 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect('mongodb://localhost:27017/chat', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+  
 const server = app.listen(process.env.PORT,()=> {
     console.log('Server Started on Port ${process.env.PORT}');
-});
+}); 
